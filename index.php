69afa488fe8369f7a2f3ef4fcfe7a81cc179996b
msabate00 <sabatemiranda@gmail.com>
2020-09-26 18:38:13 +0200

    msm - Primera función creada

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        function msm_Funcion1() {
+            echo "Primera funcion";
+        }
+        ?>
+    </body>
+</html>
